10e97eebd480c01d784d54a42dd30fa190a3abdb
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-19 01:15:06 +0600

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <div>hello</div>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/todos" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
