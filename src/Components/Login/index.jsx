ea6b44efb1141c107188ec701c46a8653a24e6fb
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-19 01:33:40 +0600

    login page completed

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -4,27 +4,27 @@ import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import useTitle from "../Utilities/useTitle";
 
-const SignUp = () => {
-    useTitle("SignUp");
+const Login = () => {
+    useTitle("Login");
     const [error, setError] = useState("");
     const location = useLocation();
     const navigate = useNavigate();
 
     const from = location.state?.from?.pathname || "/";
 
-    const signUp = (e) => {
+    const signIn = (e) => {
         e.preventDefault();
-        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        if (email && password && name) {
+        if (email && password) {
             axios
-                .post("https://todo-app-react-saad.herokuapp.com/signup", {
-                    name,
+                .post("https://todo-app-react-saad.herokuapp.com/login", {
                     email,
                     password,
                 })
                 .then(({ data }) => {
+                    console.log(data);
+
                     if (data.success) {
                         localStorage.setItem("accessToken", data.token);
                         navigate("/todos");
@@ -39,29 +39,15 @@ const SignUp = () => {
             <div className="sm:w-1/3">
                 <form
                     className="p-8 bg-slate-900 border border-gray-500/50 rounded-md shadow-md drop-shadow-lg text-slate-100 space-y-4"
-                    onSubmit={signUp}
+                    onSubmit={signIn}
                 >
-                    <h2 className="text-center text-3xl">SignUp</h2>
+                    <h2 className="text-center text-3xl">Login</h2>
                     <div className="divider" />
                     {error && (
                         <p className="p-4 mb-4 text-red-600 rounded-md">
                             {error}
                         </p>
                     )}
-                    <div className="form-control">
-                        <label
-                            className="block mb-2 text-sm font-bold "
-                            htmlFor="username"
-                        >
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Type here"
-                            className="input input-bordered input-success w-full "
-                        />
-                    </div>
                     <div className="form-control">
                         <label
                             className="block mb-2 text-sm font-bold "
@@ -93,15 +79,14 @@ const SignUp = () => {
                     <div className="space-y-4">
                         <input
                             type="submit"
-                            value="SignUp"
-                            placeholder="Type here"
+                            value="Login"
                             className="input input-bordered input-success w-full btn btn-secondary"
                         />
                     </div>
                     <div className="flex items-center justify-center mt-4">
-                        <p>Already have an account?</p>
-                        <NavLink to="/login" className="ml-1 text-green-600">
-                            login
+                        <p>Don't have an account?</p>
+                        <NavLink to="/signup" className="ml-1 text-green-600">
+                            Create New
                         </NavLink>
                     </div>
                 </form>
@@ -110,4 +95,4 @@ const SignUp = () => {
     );
 };
 
-export default SignUp;
+export default Login;
